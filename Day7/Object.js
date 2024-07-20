604d9e5ee76a652ad0c9d9e586fb9e7922b93ce3
Megha-Bathla <bathlamegha6@gmail.com>
2024-07-21 00:13:13 +0530

    Day 7 completed

--- Day7/Object.js
+++ Day7/Object.js
@@ -1,14 +1,16 @@
 //ACtivity1: Object and access
-
 const book = {
   title: "Literature",
   author: "Himanshu",
+  year: "2012 - 2023",
   age: 34,
 };
 console.log(book);
-console.log(`Title:, ${book.title} and author ${book.author}`);
+console.log(
+  `Title:, ${book.title} and author ${book.author} in year ${book.year}`
+);
 
-//Activity2: Object Method
+//Activity2: Object Method //Activity4: this keyword
 
 const Book = {
   title: "Literature",
@@ -32,12 +34,25 @@ const library = {
   name: "Arvind Kaushikh",
   books: [
     {
-      name: "Hello",
+      title: "Hello",
       notebook: "Ajanta",
     },
     {
-      name: "World map",
+      title: "World map",
       notebook: "Classic",
     },
   ],
 };
+
+console.log(library);
+
+console.log(library.name);
+console.log(library.books[0].title);
+
+library.books.forEach((book) => console.log(book.title));
+
+//Activity5: Object iteration
+
+for (const index in library.books) {
+  console.log(library.books[index].notebook);
+}
